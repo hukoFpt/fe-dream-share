c42d073eb1264436bc0205b494257210d8ef9887
hukoFpt <lynnie.nguyen.bns@gmail.com>
2024-03-06 06:00:25 +0700

    v2

---
+++ src/app/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import "./globals.css"
+
+function App() {
+    return (
+        <Router>
+            {/* Your app content goes here */}
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/app/components/navbar/Categories.tsx
@@ -0,0 +1,84 @@
+import CategoryBox from "../CategoryBox";
+import Container from "../Container";
+import { TbArmchair, TbBrandAirtable, TbBedFlat } from "react-icons/tb";
+
+export const categories = [
+  {
+    label: "Chair",
+    icon: TbArmchair,
+  },
+  {
+    label: "Table",
+    icon: TbBrandAirtable,
+  },
+  {
+    label: "Bed",
+    icon: TbBedFlat,
+  },
+  {
+    label: "Chair",
+    icon: TbArmchair,
+  },
+  {
+    label: "Table",
+    icon: TbBrandAirtable,
+  },
+  {
+    label: "Bed",
+    icon: TbBedFlat,
+  },
+  {
+    label: "Chair",
+    icon: TbArmchair,
+  },
+  {
+    label: "Table",
+    icon: TbBrandAirtable,
+  },
+  {
+    label: "Bed",
+    icon: TbBedFlat,
+  },
+  {
+    label: "Chair",
+    icon: TbArmchair,
+  },
+  {
+    label: "Table",
+    icon: TbBrandAirtable,
+  },
+  {
+    label: "Bed",
+    icon: TbBedFlat,
+  },{
+    label: "Table",
+    icon: TbBrandAirtable,
+  },
+  {
+    label: "Bed",
+    icon: TbBedFlat,
+  },
+];
+
+const Categories = () => {
+  return (
+    <Container>
+      <div
+        className="
+                pt-4
+                flex
+                flex-row
+                items-center
+                justify-between
+                overflow-x-auto
+            "
+      >
+        {categories.map((item) => (
+          <CategoryBox key={item.label} label={item.label} icon={item.icon} />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
