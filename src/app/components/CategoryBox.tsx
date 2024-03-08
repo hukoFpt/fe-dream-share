97733cd76b33eb9d6e5dfee1c94425ee708db7c9
hukoFpt <lynnie.nguyen.bns@gmail.com>
2024-03-08 10:30:16 +0700

    Fix formatting and add missing import statement

--- src/app/components/CategoryBox.tsx
+++ src/app/components/CategoryBox.tsx
@@ -15,6 +15,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
   selected,
 }) => {
+  
   const router = useRouter();
   const params = useSearchParams();
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
 "use client";
 
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from "react";
 import Product from "./components/Product";
-import Navbar from './components/navbar/Navbar';
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
 
 export default function Home() {
-  const [paddingTop, setPaddingTop] = useState('470px');
+  const [paddingTop, setPaddingTop] = useState("470px");
   const handleScroll = () => {
     // Change the padding-top value based on the scroll position
     if (window.scrollY > 50) {
-      setPaddingTop('181px');
+      setPaddingTop("181px");
     } else {
-      setPaddingTop('470px');
+      setPaddingTop("470px");
     }
   };
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
+
   return (
     <div style={{ paddingTop }}>
       <Product />
